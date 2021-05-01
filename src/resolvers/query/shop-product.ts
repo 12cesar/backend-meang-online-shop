540cb262de50dae04ecdb03ee04324afa3ccad74
unknown <julcescalgalty@gmail.com>
2021-05-01 07:38:26 -0500

    Seccion 17 finalizada

--- src/resolvers/query/shop-product.ts
+++ src/resolvers/query/shop-product.ts
@@ -2,28 +2,45 @@ import { IResolvers } from 'graphql-tools';
 import ShopProductsService from '../../services/shop-product.service';
 const resolversProductsQuery: IResolvers = {
   Query: {
-    shopProducts(_, {page, itemsPage, active}, context){
-      return new ShopProductsService(_,{pagination:{page,itemsPage}},context).items(active);
+    shopProducts(_, { page, itemsPage, active }, context) {
+      return new ShopProductsService(
+        _,
+        { pagination: { page, itemsPage } },
+        context
+      ).items(active);
     },
-    shopProductsPlatforms(_, {page, itemsPage, active, platform, random}, context){
-      return new ShopProductsService(_,{pagination:{page,itemsPage}},context).items(active, platform, random);
+    shopProductsPlatforms(
+      _,
+      { page, itemsPage, active, platform, random },
+      context
+    ) {
+      return new ShopProductsService(
+        _,
+        { pagination: { page, itemsPage } },
+        context
+      ).items(active, platform, random);
     },
-    shopProductsOffersLast(_,{page, itemsPage, active, topPrice, lastUnits, random}, context){
+    shopProductsOffersLast(
+      _,
+      { page, itemsPage, active, topPrice, lastUnits, random },
+      context
+    ) {
       let otherFilters = {};
       if (lastUnits > 0 && topPrice > 10) {
         otherFilters = {
-          $and:[
-            {price:{$lte:topPrice}},
-            {stock:{$lte:lastUnits}}
-          ]
+          $and: [{ price: { $lte: topPrice } }, { stock: { $lte: lastUnits } }],
         };
-      }else if (lastUnits <= 0 && topPrice >10) {
-        otherFilters = {price:{$lte:topPrice}};
-      }else if (lastUnits > 0 && topPrice <= 10) {
-        otherFilters = {stock:{$lte:lastUnits}};
+      } else if (lastUnits <= 0 && topPrice > 10) {
+        otherFilters = { price: { $lte: topPrice } };
+      } else if (lastUnits > 0 && topPrice <= 10) {
+        otherFilters = { stock: { $lte: lastUnits } };
       }
-      return new ShopProductsService(_,{pagination:{page,itemsPage}},context).items(active, '', random, otherFilters);
-    }
+      return new ShopProductsService(
+        _,
+        { pagination: { page, itemsPage } },
+        context
+      ).items(active, ['-1'], random, otherFilters);
+    },
   },
 };
 
--- src/services/shop-product.service.ts
+++ src/services/shop-product.service.ts
@@ -4,73 +4,76 @@ import { randomItems } from '../lib/db-operations';
 import ResolversOperationsService from './resolvers-operations.service';
 
 class ShopProductsService extends ResolversOperationsService {
-    collection = COLLECTIONS.SHOP_PRODUCT;
-    constructor(root: object, variables: object, context: IContextData) {
-        super(root, variables, context);
+  collection = COLLECTIONS.SHOP_PRODUCT;
+  constructor(root: object, variables: object, context: IContextData) {
+    super(root, variables, context);
+  }
+  async items(
+    active: string = ACTIVE_VALUES_FILTER.ACTIVE,
+    platform: Array<string> = ['-1'],
+    random: boolean = false,
+    otherFilters: object = {}
+  ) {
+    let filter: object = { active: { $ne: false } };
+    if (active === ACTIVE_VALUES_FILTER.ALL) {
+      filter = {};
+    } else if (active === ACTIVE_VALUES_FILTER.INACTIVE) {
+      filter = { active: { $eq: false } };
     }
-    async items(active: string = ACTIVE_VALUES_FILTER.ACTIVE, platform: string = '', random: boolean = false, otherFilters: object={}) {
-        let filter: object = { active: { $ne: false } };
-        if (active === ACTIVE_VALUES_FILTER.ALL) {
-            filter = {};
-        } else if (active === ACTIVE_VALUES_FILTER.INACTIVE) {
-            filter = { active: { $eq: false } };
-        }
-        if (platform !== '' && platform !== undefined) {
-            filter = { ...filter, ...{ platform_id: platform } }
-        }
-        if (otherFilters !== {} && otherFilters !== undefined) {
-            filter = { ...filter, ...otherFilters }
-        }
-        console.log(filter, 'filter');
-
-        const page = this.getVariables().pagination?.page;
-        const itemsPage = this.getVariables().pagination?.itemsPage;
-        if (!random) {
-            const result = await this.list(
-                this.collection,
-                'Productos de la tienda',
-                page,
-                itemsPage,
-                filter
-            );
-            return {
-                info: result.info,
-                status: result.status,
-                message: result.message,
-                shopProducts: result.item,
-            };
-        }
-        const result: Array<object> = await randomItems(
-            this.getDb(),
-            this.collection,
-            filter,
-            itemsPage
-        );
-        if (result.length === 0 || result.length !== itemsPage) {
-            return {
-                info: {
-                    page: 1,
-                    pages: 1,
-                    itemsPage,
-                    total: 0
-                },
-                status: false,
-                message: 'La informacion que hemos pedido no se ha obtenido tal como deseabamos',
-                shopProducts: [],
-            };
-        }
-        return {
-            info: {
-                page: 1,
-                pages: 1,
-                itemsPage,
-                total: itemsPage
-            },
-            status: false,
-            message: 'La informacion que hemos pedido se ha cargado correctamente',
-            shopProducts: result,
-        };
-
+    if (platform[0] !== '-1' && platform !== undefined) {
+      filter = { ...filter, ...{ platform_id: {$in: platform} } };
     }
+    if (otherFilters !== {} && otherFilters !== undefined) {
+      filter = { ...filter, ...otherFilters };
+    }
+    const page = this.getVariables().pagination?.page;
+    const itemsPage = this.getVariables().pagination?.itemsPage;
+    if (!random) {
+      const result = await this.list(
+        this.collection,
+        'Productos de la tienda',
+        page,
+        itemsPage,
+        filter
+      );
+      return {
+        info: result.info,
+        status: result.status,
+        message: result.message,
+        shopProducts: result.item,
+      };
+    }
+    const result: Array<object> = await randomItems(
+      this.getDb(),
+      this.collection,
+      filter,
+      itemsPage
+    );
+    if (result.length === 0 || result.length !== itemsPage) {
+      return {
+        info: {
+          page: 1,
+          pages: 1,
+          itemsPage,
+          total: 0,
+        },
+        status: false,
+        message:
+          'La informacion que hemos pedido no se ha obtenido tal como deseabamos',
+        shopProducts: [],
+      };
+    }
+    return {
+      info: {
+        page: 1,
+        pages: 1,
+        itemsPage,
+        total: itemsPage,
+      },
+      status: false,
+      message: 'La informacion que hemos pedido se ha cargado correctamente',
+      shopProducts: result,
+    };
+  }
 }
 export default ShopProductsService;
